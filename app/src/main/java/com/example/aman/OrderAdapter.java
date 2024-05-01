7cd5401aedd34e0f2969600e704c1f737474b04a
HammadiOuassim <ouassimhammadi@gmail.com>
2024-05-01 23:18:43 +0100

    refused order btn hm

--- app/src/main/java/com/example/aman/OrderAdapter.java
+++ app/src/main/java/com/example/aman/OrderAdapter.java
@@ -70,7 +70,7 @@ public class OrderAdapter extends ArrayAdapter<Order> {
         refuse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                order.setStatus("refused");
+                FirebaseDatabase.getInstance().getReference().child("orders").child("status").setValue("refused");
             }
         });
         return layout;
